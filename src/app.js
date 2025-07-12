f02f862d9ac93a4ab5740412dc4bbcd8bc336f98
Luis Gerardo Tellez Carranza <vtaejk@hotmail.com>
2025-07-12 02:17:15 +0000

    Added code for card generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import "bootstrap";
 import "./style.css";
 
+window.onload = () => {
+    document.querySelector('.card').classList.add(generateRandomSuit());
+    document.querySelector('.card').innerHTML = generateRandomNumber();
+};
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let generateRandomNumber = () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit]
 };
